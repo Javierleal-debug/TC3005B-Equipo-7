50f79fd684b0e05d14043e3020e98849152407f5
Luisarmandosl <luis-armandosl@hotmail.com>
2022-04-18 13:23:51 -0500

    Login page frontend

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,42 @@
+import { Button, Form,TextInput } from 'carbon-components-react'
 import React from 'react'
 
 const LandingPage = () => {
-  return <h1>Login Page</h1>
+  return(
+    <div>
+      <div class="loginTitle">
+        Sign in with your Peripheral Loans Credentials
+      </div>
+      <div class="loginInstructions">
+        Use your Email and Password
+      </div>
+
+      <Form>
+        <div class="textinput">
+          <TextInput
+          type="text"
+          id="email"
+          placeholder="Email here"
+          labelText="Email"
+          />
+
+        </div>
+        <div class="textinput">
+          <TextInput
+          type="password"
+          id="pwd"
+          placeholder="Password here"
+          labelText="Password"
+          />
+        </div>
+        <div class="textinput">
+          <Button>
+            Sign in
+          </Button>
+        </div>
+      </Form>
+    </div>
+  )
 }
 
 export default LandingPage
