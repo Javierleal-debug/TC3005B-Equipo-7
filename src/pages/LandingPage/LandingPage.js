c97ef674a1118bb8ed12165d9e7adf7f6b92dae0
ACMR1009 <anacristina.munguia@gmail.com>
2022-04-23 00:25:37 -0500

    Add devices table view (front-end)
    - New table view (non-functional)
    - Styles for new view
    - Corrected typo ("periferal" => "peripheral")
    
    Co-Authored-By: Carlos-Ojeda <a01741085@tec.mx>

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -6,7 +6,7 @@ const LandingPage = () => {
   return (
     <Grid className="home-banner">
       <Column sm={4} md={8} lg={16} className="page-title-block">
-        <h1>Periferal Loans</h1>
+        <h1>Peripheral Loans</h1>
       </Column>
     </Grid>
   )
