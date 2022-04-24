e3ac4c5954830da43c9699d00f95eb33dfbf2adc
Marian Herrera <64176067+saalej@users.noreply.github.com>
2022-04-24 02:29:52 -0500

    Dashboard falta scss

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import { GlobalTheme } from 'carbon-components-react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <BrowserRouter>
       <GlobalTheme>
         <App />
       </GlobalTheme>
     </BrowserRouter>
-  </React.StrictMode>
+  //</React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
