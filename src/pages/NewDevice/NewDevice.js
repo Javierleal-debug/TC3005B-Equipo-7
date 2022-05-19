0c7f80551df290c897c8950144d4e6715a449f33
ACMR1009 <anacristina.munguia@gmail.com>
2022-05-19 18:48:11 -0500

    Add dropdown feature in New Device page
    - Dropdown feature for device type selection.
    - New Employee Name, Email & Serial Number fields.
    - New Comment field.
    
    Co-Authored-By: Carlos-Ojeda <a01741085@tec.mx>

--- src/pages/NewDevice/NewDevice.js
+++ src/pages/NewDevice/NewDevice.js
@@ -11,8 +11,40 @@ import {
   Form,
   Stack,
   TextInput,
+  TextArea,
+  Dropdown
 } from 'carbon-components-react'
 import { Misuse, Save } from '@carbon/icons-react'
+
+const items = [
+  "Monitor",
+  "Mouse",
+  "Keyboard",
+  "Headset",
+  "Adapter",
+  "Computer",
+  "Tablet",
+  "Laptop",
+  "Touchscreen RB Pi",
+  "Camera / Webcam",
+  "Personal labeler",
+  "OTP Hardware",
+  "Mobile Phone",
+  "Virtual Assistant",
+  "Trackpad",
+  "Power Adapter / Charger",
+  "Apple TV",
+  "Articulating Mount",
+  "HDD / SSD",
+  "HDMI Cable",
+  "USB-C Charge cable",
+  "USB Memory",
+  "Combo Keyboard Mouse",
+  "Multiport Adapter",
+  "Video Adapter",
+  "Docking Station",
+  "Others"
+]
 
 const deviceData={
 }
@@ -20,7 +52,7 @@ const deviceData={
 const NewDevice = () => {
 
   const handleTypeChange = (event) => {
-    deviceData.type = event.target.value;
+    deviceData.type = event.selectedItem;
   }
 
   const handleBrandChange = (event) => {
@@ -35,6 +67,22 @@ const NewDevice = () => {
     deviceData.serial = event.target.value;
   }
 
+  const handleEmployeeNameChange = (event) => {
+    deviceData.employeeName = event.target.value;
+  }
+
+  const handleEmployeeEmailChange = (event) => {
+    deviceData.employeeEmail = event.target.value;
+  }
+
+  const handleEmployeeSerialChange = (event) => {
+    deviceData.employeeSerial = event.target.value;
+  }
+
+  const handleCommentChange = (event) => {
+    deviceData.comment = event.target.value;
+  }
+
   const createItemRequest = () => {
 
     var userInfo = JSON.parse(localStorage.getItem('UserInfo'));
@@ -45,7 +93,10 @@ const NewDevice = () => {
       serialNumber: deviceData.serial,
       acceptedConditions: false,
       isInside: true,
-      securityAuthorization: false
+      securityAuthorization: false,
+      employeeName: deviceData.employeeName,
+      employeeEmail: deviceData.employeeEmail,
+      employeeSerial: deviceData.employeeSerial
     };
     let requestHeaders = {
       headers: {
@@ -64,15 +115,25 @@ const NewDevice = () => {
 
   return (
     <>
+      <h1>New Device</h1>
       <Grid className="page-content">
-        <Column sm={4} md={8} lg={16} className="table-block">
-          <h1>New Device</h1>
+        <Column sm={4} md={8} lg={8} className="table-block">
           <Form>
-            <Stack gap={4}>
-              <TextInput id="Type" onChange={handleTypeChange} placeholder="Device Type" labelText="Type" />
+            <Stack>
+              <Dropdown id="Type" onChange={handleTypeChange} label="Select Device Type" titleText="Type" items={items} />
               <TextInput id="Brand" onChange={handleBrandChange} placeholder="Device Brand" labelText="Brand" />
               <TextInput id="Model" onChange={handleModelChange} placeholder="Model" labelText="Model" />
               <TextInput id="Serial" onChange={handleSerialChange} placeholder="Serial Number" labelText="Serial" />
+              <TextInput id="EmployeeName" onChange={handleEmployeeNameChange} placeholder="Requisitor's Name" labelText="Employee Name" />
+            </Stack>
+          </Form>
+        </Column>
+        <Column sm={4} md={8} lg={8}>
+          <Form>
+            <Stack>
+              <TextInput id="EmployeeEmail" onChange={handleEmployeeEmailChange} placeholder="Requisitor's Email" labelText="Employee Email" />
+              <TextInput id="EmployeeSerial" onChange={handleEmployeeSerialChange} placeholder="Requisitor's Serial Number" labelText="Employee Serial" />
+              <TextArea id='Comment' onChange={handleCommentChange} placeholder="Optional Comment" labelText="Comment" rows={6}/>
               <ButtonSet className="edit-mode-button-set">
                 <Button
                   renderIcon={Misuse}
@@ -93,7 +154,9 @@ const NewDevice = () => {
             </Stack>
           </Form>
         </Column>
+        
       </Grid>
+      
     </>
   )
 }
