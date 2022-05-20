6869b4f254f32c911f2b296ed903e0b504de64b5
oscar-me <oscar-me@outlook.com>
2022-05-19 21:19:16 -0500

    format

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,82 +1,71 @@
-import { Button, Form,TextInput } from 'carbon-components-react'
+import { Button, Form, TextInput } from 'carbon-components-react'
 import React from 'react'
-import {Link,useHref,useNavigate} from 'react-router-dom'
 
-
-
-
-const state={
-  email:'',
-  pwd:''
+const state = {
+  email: '',
+  pwd: '',
 }
 
-function signIn(){
-  //alert(`inicio de sesion ${state.email} ${state.pwd}`)
-  fetch("https://peripheralsloanbackend.mybluemix.net/auth/login", {
-        method: 'POST',
-        body: JSON.stringify({
-            email: state.email,
-            pwd: state.pwd}),
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8'
-        }
-    })
-    .then(response => response.json())
-    .then(json => {
-      console.log(json.accessToken);
-      if (json.accessToken){
-        localStorage.setItem('UserInfo',JSON.stringify(json));
-        window.location.hash="/devices"
-      }else{
-        alert("Wrong user or password");
-        
+function signIn() {
+  fetch('https://peripheralsloanbackend.mybluemix.net/auth/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      email: state.email,
+      pwd: state.pwd,
+    }),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json.accessToken)
+      if (json.accessToken) {
+        localStorage.setItem('UserInfo', JSON.stringify(json))
+        window.location.hash = '/devices'
+      } else {
+        alert('Wrong user or password')
       }
-    });
+    })
 }
 
-function handleChangeEmail(event){
-  state.email=event.target.value
+function handleChangeEmail(event) {
+  state.email = event.target.value
 }
 
-function handleChangePwd(event){
-  state.pwd=event.target.value
+function handleChangePwd(event) {
+  state.pwd = event.target.value
 }
 
 const LandingPage = () => {
-  return(
+  return (
     <div>
       <div class="loginTitle">
         Sign in with your Peripheral Loans Credentials
       </div>
-      <div class="loginInstructions">
-        Use your Email and Password
-      </div>
+      <div class="loginInstructions">Use your Email and Password</div>
 
       <Form>
         <div class="textinput">
           <TextInput
-          onChange={handleChangeEmail}
-          type="text"
-          id="email"
-          placeholder="Email here"
-          labelText="Email"
+            onChange={handleChangeEmail}
+            type="text"
+            id="email"
+            placeholder="Email here"
+            labelText="Email"
           />
-
         </div>
         <div class="textinput">
           <TextInput.PasswordInput
-          onChange={handleChangePwd}
-          type="password"
-          id="pwd"
-          placeholder="Password here"
-          labelText="Password"
+            onChange={handleChangePwd}
+            type="password"
+            id="pwd"
+            placeholder="Password here"
+            labelText="Password"
           />
         </div>
         <div class="textinput">
-          <Button
-          onClick={signIn}>
-            Sign in
-          </Button>
+          <Button onClick={signIn}>Sign in</Button>
         </div>
       </Form>
     </div>
