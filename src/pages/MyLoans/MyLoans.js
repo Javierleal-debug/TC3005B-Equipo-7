9a14fef7b25037e7722a7b1b97661281011a84d1
Carlos-Ojeda <A01741085@itesm.mx>
2022-05-26 18:11:47 -0600

    Add "My Loans" page
    Still working on that
    
    Co-Authored-By: ACMR1009 <46897998+ACMR1009@users.noreply.github.com>

---
+++ src/pages/MyLoans/MyLoans.js
@@ -0,0 +1,281 @@
+import axios from 'axios'
+import tableHeaders from './headers.json'
+import React, { useState, useEffect } from 'react'
+import { FlagFilled } from '@carbon/icons-react'
+import {
+  Button,
+  DataTableSkeleton,
+  DataTable,
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableBody,
+  TableCell,
+  TableToolbar,
+  TableToolbarContent,
+  TableToolbarSearch,
+  TableBatchActions,
+  TableBatchAction,
+  TableSelectAll,
+  TableSelectRow,
+  Pagination,
+  SkeletonText,
+  Tag,
+} from 'carbon-components-react'
+
+import { checkAuth, getDeviceStatus } from '../../util'
+
+import { useSessionData } from '../../global-context'
+
+const devices = [{}]
+
+const MyLoans = () => {
+  const [loadingData, setLoadingData] = useState(true)
+  const [searchingData, setSearchingData] = useState(false)
+
+  const [rows, setRows] = useState(null)
+  const headers = tableHeaders
+
+  let itemsPerPage = 10
+  let pageNumber = 1
+  const [pageConfig, setPageConfig] = useState([itemsPerPage, pageNumber])
+
+  const loadRows = (
+    firstItemIndex = itemsPerPage * (pageNumber - 1),
+    lastItemIndex = itemsPerPage * pageNumber
+  ) => {
+    /*let firstItemIndex = (itemsPerPage)*(pageNumber-1);
+    let lastItemIndex = itemsPerPage*pageNumber;*/
+    if (lastItemIndex > devices.length) {
+      lastItemIndex = devices.length
+    }
+    const subArray = devices.slice(firstItemIndex, lastItemIndex)
+
+    setRows(subArray)
+    setLoadingData(false)
+  }
+
+  const getItemsRequest = () => {
+    var userInfo = JSON.parse(localStorage.getItem('UserInfo'))
+    var requestRowData = {
+      headers: {
+        'x-access-token': `${userInfo['accessToken']}`,
+      },
+    }
+
+    axios
+      .get(
+        'https://peripheralsloanbackend.mybluemix.net/peripheral/',
+        requestRowData
+      )
+      .then(({ data }) => {
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+          var newRow = {
+            id: (i + 1).toString(),
+            type: data[i].type,
+            brand: data[i].brand,
+            model: data[i].model,
+            serialNumber: data[i].serialNumber,
+            loanStatus: `${getDeviceStatus(
+              data[i].acceptedConditions,
+              data[i].isInside,
+              data[i].securityAuthorization,
+              data[i].employeeName
+            )}`,
+            date: `${
+              data[i].employeeName === '' ? (data[i].employeeName = 'No one') : data[i].employeeName
+            }`,
+          }
+          devices[i] = newRow
+        }
+        loadRows()
+      })
+  }
+
+  const { sessionData, setSessionData } = useSessionData()
+
+  useEffect(() => {
+    try {
+      JSON.parse(localStorage.getItem('UserInfo'))
+      checkAuth(sessionData, setSessionData)
+      getItemsRequest()
+    } catch (e) {
+      window.location.hash = '/login'
+    }
+    // eslint-disable-next-line
+  }, [])
+
+  function handleChangeItemsPerPage(event) {
+    itemsPerPage = event.pageSize
+    pageNumber = event.page
+    setPageConfig([itemsPerPage, pageNumber])
+    loadRows()
+  }
+
+  const batchActionClick = (selectedRows) => {
+    let serialNumbers = []
+    selectedRows.forEach((i) => {
+      let serialNumber = i.cells[3].value
+      serialNumbers.push(serialNumber)
+    })
+    console.log(serialNumbers)
+
+    var userInfo = JSON.parse(localStorage.getItem('UserInfo'))
+    var requestData = {
+      headers: {
+        'x-access-token': `${userInfo['accessToken']}`,
+      },
+      data: {
+        array: serialNumbers,
+      },
+    }
+    axios
+      .delete(
+        'https://peripheralsloanbackend.mybluemix.net/peripheral/',
+        requestData
+      )
+      .then(({ data }) => {
+        window.location.reload()
+      })
+  }
+
+  const createCellOfType = (cell, row) => {
+    if (cell.value === 'Available') {
+      return (
+        <div>
+          <Tag renderIcon={FlagFilled} size="md" className="icon-check">
+            {cell.value}
+          </Tag>
+        </div>
+      )
+    } else if (cell.value === 'Borrowed') {
+      return (
+        <div>
+          <Tag renderIcon={FlagFilled} size="md" className="icon-fail">
+            {cell.value}
+          </Tag>
+        </div>
+      )
+    } else if (cell.value === 'Requested') {
+      return (
+        <div>
+          <Tag renderIcon={FlagFilled} size="md" className="icon-warning">
+            {cell.value}
+          </Tag>
+        </div>
+      )
+    } else if (cell.value === 'Invalid') {
+      return (
+        <div>
+          <Tag renderIcon={FlagFilled} size="md" className="icon-invalid">
+            {cell.value}
+          </Tag>
+        </div>
+      )
+    } else {
+      if ('type' === cell.id.split(':')[1]) {
+        var pathString = '#/devices/' + row.cells[3].value
+        return <a href={pathString}>{cell.value}</a>
+      }
+      if ('currentUser' === cell.id.split(':')[1]) {
+        return <div>{cell.value}</div>
+      }
+      return cell.value
+    }
+  }
+
+  return loadingData === true ? (
+    <div className="loadingComponent">
+      <DataTableSkeleton columnCount={6} rowCount={10} />
+      <SkeletonText heading={true} />
+    </div>
+  ) : (
+    <DataTable
+      rows={rows}
+      headers={headers}
+      isSortable
+      render={({
+        rows,
+        headers,
+        getHeaderProps,
+        getToolbarProps,
+        getRowProps,
+        onInputChange,
+        selectedRows,
+        getTableProps,
+        getTableContainerProps,
+      }) => (
+        <TableContainer title="My Device List" {...getTableContainerProps()}>
+          <TableToolbar {...getToolbarProps()}>
+            <TableToolbarContent>
+              <TableToolbarSearch
+                onChange={(event) => {
+                  if (event.target.value === '') {
+                    itemsPerPage = pageConfig[0]
+                    pageNumber = pageConfig[1]
+                    setSearchingData(false)
+                    loadRows()
+                  } else {
+                    setSearchingData(true)
+                    loadRows(0, devices.length)
+                  }
+                  onInputChange(event)
+                }}
+              />
+            </TableToolbarContent>
+          </TableToolbar>
+
+          <Table {...getTableProps()}>
+            <TableHead>
+              <TableRow className="table-row">
+                {headers.map((header) => (
+                  <TableHeader
+                    className="header"
+                    key={header.key}
+                    {...getHeaderProps({ header })}
+                  >
+                    <div className="table-header">{header.header}</div>
+                  </TableHeader>
+                ))}
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {rows.map((row) => (
+                <React.Fragment key={row.id}>
+                  <TableRow {...getRowProps({ row })} className="table-row">
+                    {row.cells.map((cell) => (
+                      <TableCell key={cell.id} className="cell2">
+                        {createCellOfType(cell, row)}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </React.Fragment>
+              ))}
+            </TableBody>
+          </Table>
+          {searchingData ? (
+            () => {}
+          ) : (
+            <Pagination
+              backwardText="Previous page"
+              forwardText="Next page"
+              itemsPerPageText="Items per page:"
+              onChange={handleChangeItemsPerPage}
+              page={pageConfig[1]}
+              pageSize={pageConfig[0]}
+              pageSizes={[10, 20, 30, 40, 50, 100]}
+              size="md"
+              totalItems={devices.length}
+            />
+          )}
+        </TableContainer>
+      )}
+    />
+  )
+}
+
+export default MyLoans
