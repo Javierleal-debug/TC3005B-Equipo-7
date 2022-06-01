952854671285f0e40f7103c0b71b0c920be07127
oscar-me <oscar-me@outlook.com>
2022-06-01 16:42:17 -0500

    user agreement confirmation

--- src/pages/UserAgreementConfirmation/UserAgreementConfirmation.js
+++ src/pages/UserAgreementConfirmation/UserAgreementConfirmation.js
@@ -1,14 +1,29 @@
-import { InlineNotification } from 'carbon-components-react'
+import { InlineNotification, Loading } from 'carbon-components-react'
 import React, { useEffect, useState } from 'react'
+import QRCode from 'react-qr-code'
 import { useParams } from 'react-router-dom'
 
 function UserAgreementConfirmation() {
   const { serialNumber } = useParams()
   const [isDataLoading, setIsDataLoading] = useState(false)
+  const [success, setSuccess] = useState(true)
 
   const requestConfirmation = async () => {
     setIsDataLoading(true)
-    // TODO fetch a ruta de confirmación
+
+    try {
+      const res = await fetch(
+        `https://peripheral-loans-equipo7.mybluemix.net/accept/${serialNumber}`
+      )
+      const resJSON = await res.json()
+      console.log(resJSON.message)
+
+      if (resJSON.message !== 'Success') setSuccess(false)
+    } catch (e) {
+      console.log(e)
+      setSuccess(false)
+    }
+
     setIsDataLoading(false)
   }
 
@@ -18,9 +33,39 @@ function UserAgreementConfirmation() {
   }, [])
 
   return (
-    <>
-      <InlineNotification></InlineNotification>
-    </>
+    // TODO notifications
+    <div className="confirmation-container">
+      <h1>Peripheral Loan Request</h1>
+
+      {isDataLoading ? (
+        <Loading />
+      ) : success ? (
+        <div className="loan-data-container confirmation-success">
+          <QRCode
+            value={`https://peripheral-loans-equipo7.mybluemix.net/#/devices/${serialNumber}`}
+            size={194}
+            className="confirmation-qr"
+          />
+
+          <p>You successfuly accepted the user agreement.</p>
+          <p>
+            Please contact the corresponding focal to finish the process and get
+            the requested device.
+          </p>
+          <p>
+            Show this QR code (it's also in the email you got) to security when
+            required.{' '}
+          </p>
+        </div>
+      ) : (
+        <div className="loan-data-container confirmation-fail">
+          <p>
+            Something went wrong. Please contact the administrator to solve this
+            issue.
+          </p>
+        </div>
+      )}
+    </div>
   )
 }
 
